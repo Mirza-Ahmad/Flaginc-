84856349c98ba906a26efeee53ad1f4caf3be1f4
Mirza-Ahmad <70958597+Mirza-Ahmad@users.noreply.github.com>
2020-10-26 22:54:32 +0500

    Added a new project

---
+++ flaginc.cpp
@@ -0,0 +1,170 @@
+#include<iostream>
+/*FLAG CREATED BY MIRZA AHMAD AWAIS FURTEHR INFORMATION CONTACT WITH ME.*/
+using namespace std;
+int main()
+{
+for(int i=1; i<=15; i++)
+{
+for(int j=1; j<=30; j++)
+{
+if(i==1 || i==15 || j==1 || j==30)
+{
+cout<<"*";
+}
+else if(i==7 && j==13)
+{
+cout<<"*";
+}
+else if(i==6 && j==13)
+{
+cout<<"*";
+}
+else if(i==8 && j==13)
+{
+cout<<"*";
+}
+else if(i==5 && j==14)
+{
+cout<<"*";
+}
+else if(i==4 && j==15)
+{
+cout<<"*";
+}
+else if(i==3 && j==16)
+{
+cout<<"*";
+}
+else if(i==9 && j==14)
+{
+cout<<"*";
+}
+else if(i==10 && j==15)
+{
+cout<<"*";
+}
+else if(i==10 && j==16)
+{
+cout<<"*";
+}
+else if(i==9 && j==15)
+{
+cout<<"*";
+}
+else if(i==8 && j==14)
+{
+cout<<"*";
+}
+else if(i==7 && j==14)
+{
+cout<<"*";
+}
+else if(i==6 && j==14)
+{
+cout<<"*";
+}
+else if(i==5 && j==15)
+{
+cout<<"*";
+}
+else if(i==4 && j==15)
+{
+cout<<"*";
+}
+else if(i==3 && j==17)
+{
+cout<<"*";
+}
+else if(i==10 && j==16)
+{
+cout<<"*";
+}
+else if(i==11 && j==17)
+{
+cout<<"*";
+}
+else if(i==8 && j==17)
+{
+cout<<"*";
+}
+else if(i==8 && j==21)
+{
+cout<<"*";
+}
+else if(i==7 && j==18)
+{
+cout<<"*";
+}
+else if(i==7 && j==19)
+{
+cout<<"*";
+}
+else if(i==7 && j==20)
+{
+cout<<"*";
+}
+else if(i==6 && j==17)
+{
+cout<<"*";
+}
+else if(i==6 && j==18)
+{
+cout<<"*";
+}
+else if(i==6 && j==20)
+{
+cout<<"*";
+}
+else if(i==6 && j==21)
+{
+cout<<"*";
+}
+else if(i==5 && j==19)
+{
+cout<<"*";
+}
+else if(i==1 || j==8)
+{
+cout<<"Û";
+}
+else if(i==1 || j==2)
+{
+cout<<"Û";
+}
+else if(i==1 || j==3)
+{
+cout<<"Û";
+}
+else if(i==1 || j==4)
+{
+cout<<"Û";
+}
+else if(i==1 || j==5)
+{
+cout<<"Û";
+}
+else if(i==1 || j==6)
+{
+cout<<"Û";
+}
+else if(i==1 || j==7)
+{
+cout<<"Û";
+}
+else if(i==1 || j==9)
+{
+cout<<"Û";
+}
+else
+{
+cout<<" ";
+}
+}
+cout<<endl;
+}
+for(int k=1; k<=15; k++)
+{
+cout<<"**"<<endl;
+}
+return 0;
+}
